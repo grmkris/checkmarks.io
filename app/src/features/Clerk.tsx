dccde2f5a96ff7fba345fa6c71f52875832c722d
grmkris <kristjan.grm1@gmail.com>
2023-05-12 16:57:03 +0100

    fixed pnpm-workspace.yaml

--- app/src/features/Clerk.tsx
+++ app/src/features/Clerk.tsx
@@ -1,13 +1,6 @@
 import {UserButton} from "@clerk/nextjs";
-import {api} from "../utils/api";
-import {useState} from "react";
 
 export const Clerk = () => {
-    const [text, setText] = useState("");
-    const hello = api.example.hello.useQuery({text1 : text}, {enabled: text.length > 10});
-    const hello = api.example.helloMutation.useMutation();
-
-    hello.isLoading && <div>Loading...</div>;
     return (
         <UserButton
             afterSignOutUrl="/"
