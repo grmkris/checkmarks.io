f1a6f89db640751fb6c27d4f41ed3b55a6b5fc96
Guillermo Tala <guille86598@gmail.com>
2023-05-13 14:53:48 +0100

    Fix: we3Wallet

--- app/src/features/iden3/useIden3DID.tsx
+++ app/src/features/iden3/useIden3DID.tsx
@@ -5,13 +5,13 @@ import { getUserDID } from "./user-did-iden3";
 export const useIden3DID = () => {
   const { user } = useUser();
   return useQuery({
-    queryKey: ["identity", user?.web3Wallets[0].web3Wallet],
+    queryKey: ["identity", user?.web3Wallets[0]?.web3Wallet],
     queryFn: async () => {
-      if (!user?.web3Wallets[0].web3Wallet) throw new Error("No wallet");
+      if (!user?.web3Wallets[0]?.web3Wallet) throw new Error("No wallet");
       return getUserDID({
-        username: user?.web3Wallets[0].web3Wallet,
+        username: user?.web3Wallets[0]?.web3Wallet,
       });
     },
-    enabled: !!user?.web3Wallets[0].web3Wallet,
+    enabled: !!user?.web3Wallets[0]?.web3Wallet,
   });
 };
