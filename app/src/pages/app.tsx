0e04b2be3454db5054e7e9c4c97e6054d6456607
grmkris <kristjan.grm1@gmail.com>
2023-05-13 16:46:34 +0100

    new stuff

--- app/src/pages/app.tsx
+++ app/src/pages/app.tsx
@@ -4,6 +4,7 @@ import { useIden3DID } from "../features/iden3/useIden3DID";
 
 const ConnectedAccounts = () => {
   const { user } = useUser();
+  console.log("username", user?.username);
   if (!user?.externalAccounts) return <div>No accounts</div>;
   return (
     <div>
@@ -23,7 +24,6 @@ export default function App() {
       <div className="container mx-auto flex min-h-screen max-w-4xl flex-col items-center justify-center bg-base-100">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <ConnectedAccounts />
-          <ToggleTheme />
           Imagine lots of cool stuff here
         </div>
       </div>
