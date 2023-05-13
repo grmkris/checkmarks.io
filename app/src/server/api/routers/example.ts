bcff1a5a8b63a41f76f160b6672d272b426b3804
Guillermo Tala <guille86598@gmail.com>
2023-05-13 02:02:18 +0100

    Feat: show connected aacounts

--- app/src/server/api/routers/example.ts
+++ app/src/server/api/routers/example.ts
@@ -1,38 +1,37 @@
-import {z} from "zod";
+import { z } from "zod";
 
-import {createTRPCRouter, protectedProcedure, publicProcedure} from "../trpc";
-import {discordConnector} from "./discordConnector";
-import {githubConnector} from "./githubConnector";
-import {tiktokConnector} from "./tiktokConnector";
+import { createTRPCRouter, protectedProcedure, publicProcedure } from "../trpc";
+import { discordConnector } from "./discordConnector";
+import { githubConnector } from "./githubConnector";
+import { tiktokConnector } from "./tiktokConnector";
 
 const helloEndpoint = publicProcedure
-    .input(z.object({text: z.string()}))
-    .output(z.object({greeting: z.string()}))
-    .query(({input}) => {
-        return {
-            greeting: `Hello ${input.text}`,
-        };
-    });
+  .input(z.object({ text: z.string() }))
+  .output(z.object({ greeting: z.string() }))
+  .query(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`,
+    };
+  });
 
 const helloMutationEndpoint = publicProcedure
-    .input(z.object({text: z.string()}))
-    .mutation(({input}) => {
-        return {
-            greeting: `Hello ${input.text}`,
-        };
-    });
+  .input(z.object({ text: z.string() }))
+  .mutation(({ input }) => {
+    return {
+      greeting: `Hello ${input.text}`,
+    };
+  });
 
-const getAllEndpoint = protectedProcedure.query(async ({ctx}) => {
-    console.log("HERE ");
-
-    return "hello";
+const getAllEndpoint = protectedProcedure.query(async ({ ctx }) => {
+  console.log("HERE ");
+  return "hello";
 });
 
 export const exampleRouter = createTRPCRouter({
-    hello: helloEndpoint,
-    helloMutation: helloMutationEndpoint,
-    getAll: getAllEndpoint,
-    connectDiscord: discordConnector,
-    connectGithub: githubConnector,
-    connectTiktok: tiktokConnector,
+  hello: helloEndpoint,
+  helloMutation: helloMutationEndpoint,
+  getAll: getAllEndpoint,
+  connectDiscord: discordConnector,
+  connectGithub: githubConnector,
+  connectTiktok: tiktokConnector,
 });
