a18c6658e80692aa03ea713c173b6aab2c18c68d
Guillermo Tala <guille86598@gmail.com>
2023-05-14 01:08:56 +0100

    fix tab center

--- app/src/pages/socials.tsx
+++ app/src/pages/socials.tsx
@@ -1,6 +1,5 @@
 import { SocialNames, useModalStore } from "../features/modals/useModalStore";
 import { useUser } from "@clerk/nextjs";
-import { Loading } from "../components/Loading";
 import { useWeb2Web3Selector } from "../features/web2Web3SelectorStore";
 
 export interface Social {
@@ -9,7 +8,7 @@ export interface Social {
 
 const Socials = () => {
   const user = useUser();
-  const { selected, setSelected } = useWeb2Web3Selector((state) => state);
+  const { selected } = useWeb2Web3Selector((state) => state);
 
   const web2SocialsButtons: Social[] = [
     { name: SocialNames.Facebook },
@@ -27,47 +26,27 @@ const Socials = () => {
 
   const openModal = useModalStore((state) => state.openModal);
 
-  console.log("qweqwe", user.user?.externalAccounts);
-  const displayOnboardingText = user.user?.externalAccounts.length === 0;
-
-  if (!user.isLoaded)
-    return (
-      <div className="hero min-h-screen">
-        <Loading />
-      </div>
-    );
-
   return (
     <div className="hero min-h-screen">
       <div className="hero-overlay bg-base-100"></div>
       <div className="hero-content text-center text-neutral-content">
         <div className="flex flex-col">
-          {displayOnboardingText && (
-            <div className="py-2">
-              <h1 className="text-5xl font-bold">Welcome to Checkmarks!</h1>
-              <p className="py-4">
-                Unleash the power of account verification by clicking that
-                button below!
-              </p>
-            </div>
+          {(selected === "web2" ? web2SocialsButtons : web3SocialsButtons).map(
+            (social, index) => {
+              return (
+                <div key={index} className="py-2">
+                  <button
+                    onClick={() =>
+                      openModal({ data: undefined, view: social.name })
+                    }
+                    className="btn-primary btn w-80"
+                  >
+                    {social.name}
+                  </button>
+                </div>
+              );
+            }
           )}
-          {!displayOnboardingText &&
-            (selected === "web2" ? web2SocialsButtons : web3SocialsButtons).map(
-              (social, index) => {
-                return (
-                  <div key={index} className="py-2">
-                    <button
-                      onClick={() =>
-                        openModal({ data: undefined, view: social.name })
-                      }
-                      className="btn-primary btn w-80"
-                    >
-                      {social.name}
-                    </button>
-                  </div>
-                );
-              }
-            )}
         </div>
       </div>
     </div>
