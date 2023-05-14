536b498b9c2d32fe965330c030a2b93f724d886c
Guillermo Tala <guille86598@gmail.com>
2023-05-14 04:19:02 +0100

    background

--- app/src/pages/index.tsx
+++ app/src/pages/index.tsx
@@ -9,17 +9,32 @@ const Home: NextPage = () => {
   if (user.isSignedIn) router.push("/socials");
 
   return (
-    <div className="hero min-h-screen">
-      <div className="hero-overlay bg-base-100"></div>
+    <div
+      className="hero min-h-screen"
+      style={{
+        backgroundImage: `url("/background_mobile.png")`,
+        backgroundSize: "auto 125%",
+        backgroundPosition: "bottom ",
+      }}
+    >
       <div className="hero-content text-center text-neutral-content">
         <div className="max-w-md">
           <h1 className="mb-5 text-5xl text-accent">
-            Get your verified credentials here
+            One check, <br></br> infinite trust{" "}
           </h1>
 
+<<<<<<< HEAD
           <SignInButton mode="modal">
             <button className="btn-primary btn">Connect your wallet</button>
           </SignInButton>
+=======
+          <button
+            className="btn-primary btn space-y-2 p-3 px-10 "
+            onClick={() => router.push("/connections#/web3-wallet ")}
+          >
+            connect <br></br>your wallet
+          </button>
+>>>>>>> 2a3e0aa (styles)
         </div>
       </div>
     </div>
