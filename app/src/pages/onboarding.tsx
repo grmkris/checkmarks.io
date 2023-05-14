761c2eb6d9db2e900c89e0ee8628da9f2a82f6c5
Guillermo Tala <guille86598@gmail.com>
2023-05-14 08:31:40 +0100

    onboarding page

--- app/src/pages/onboarding.tsx
+++ app/src/pages/onboarding.tsx
@@ -1,7 +1,14 @@
 const Onboarding = () => {
   return (
     <div className="hero min-h-screen">
-      <div>ONBOARDING</div>
+      <div className="hero-content text-center text-neutral-content">
+        <div className="max-w-md">
+          <h1 className="mb-5 text-3xl text-neutral">
+            Unleash the power of account verification by clicking that button in
+            the right corner.
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
